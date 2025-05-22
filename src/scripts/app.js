f9252b276a22b4194f1714e3671aa367efa4d864
thomasbrh <thomas.bruch@student.heaj.be>
2025-05-22 03:11:38 +0200

    Update img, cursor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -25,6 +25,7 @@ function updateProgressBar() {
     }
 }
 
+
 /* Etat actif de la barre de navigation mobile */
 document.querySelectorAll('.page').forEach(element => {
     element.addEventListener('click', (event) => {
@@ -37,3 +38,28 @@ document.querySelectorAll('.page').forEach(element => {
         event.currentTarget.classList.add('page--active');
     });
 });
+
+
+/* cursor */
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll(".link");
+
+window.addEventListener("mousemove", (e) => {
+  
+    let x = e.pageX;
+    let y = e.pageY;
+  
+    cursor.forEach(el => {
+        el.style.left = `${x}px`;
+        el.style.top = `${y}px`;
+    
+    links.forEach(link => {
+        link.addEventListener("mouseenter", () => {
+        el.classList.add("hover");
+    })
+
+        link.addEventListener("mouseleave", () => {
+        el.classList.remove("hover");
+    })})
+  })
+})
