dab34ce7dc11f8b9b702235520c56b8ff870e22e
thomasbrh <thomas.bruch@student.heaj.be>
2025-08-22 16:17:29 +0200

    Erreur console slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,9 +3,4 @@ import './breakpoints.js';
 import './navigation.js';
 import './cursor.js';
 import './animations.js';
-
-
-// Import conditionnel du slider uniquement sur prux.html
-if (window.location.pathname.includes('prux.html')) {
-    import('./slider.js');
-}
+import './slider.js';
